a834b5888fef8cd422622d216a3359aeb5b31a1d
Anurag kharwar <88261615+AnuragKharwar@users.noreply.github.com>
2022-03-15 09:45:23 +0530

    Add files via upload

---
+++ BasicsOfProgramming/Level2/pattern1_lowertriangle.cpp
@@ -0,0 +1,26 @@
+// *
+// *	*
+// *	*	*
+// *	*	*	*
+// *	*	*	*	*
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cin >> n;
+
+    // write your code here
+    for (int j = 0; j < n; j++)
+    {
+        int i = 0;
+        while (i <= j)
+        {
+            cout << "*\t";
+            i++;
+        }
+        cout << endl;
+    }
+}
